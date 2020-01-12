5b4d90d66df1a20ae52a42d48167563deef4155f
Sergio Guastaferro <sergio.guastaferro@yahoo.it>
2020-01-12 13:32:30 +0100

    Fix DevKit config.sh

--- DevKit/config.sh
+++ DevKit/config.sh
@@ -9,3 +9,7 @@ find $DIRCONFIG_FILE/../ -maxdepth 2 -iname "*.cpp" -exec cp {} $DIRCONFIG_FILE/
 echo "Coping all include files in 'include' path..."
 echo "find $DIRCONFIG_FILE/../ -maxdepth 2 -iname "*.h" -exec cp {} $DIRCONFIG_FILE/$INCLUDE/ \;"
 find $DIRCONFIG_FILE/../ -maxdepth 2 -iname "*.h" -exec cp {} $DIRCONFIG_FILE/$INCLUDE/ \;
+
+echo "Removing module and main from src..."
+echo "rm $IDIR/module.cpp $IDIR/main.cpp $INCLUDE/module.h"
+rm $IDIR/module.cpp $IDIR/main.cpp $INCLUDE/module.h
